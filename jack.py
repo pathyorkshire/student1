d5e81f5ecacabde96adc306c4b15b6f6856f8cf7
pathyorkshire <jack.so@pathyorkshire.co.uk>
2024-09-09 16:37:13 +0100

    Update jack.py

--- jack.py
+++ jack.py
@@ -1,9 +1,8 @@
 import streamlit as st
-import plotly.express as px
-import pandas as pd
-
-data = px.data.gapminder()
-fig = px.scatter(data, x="gdpPercap", y="lifeExp", animation_frame="year", animation_group="country",
-                 size="pop", color="continent", hover_name="country", log_x=True, size_max=60,
-                 range_x=[100,100000], range_y=[25,90])
-st.plotly_chart(fig)
+import time
+seconds = st.number_input("Enter countdown time in seconds:", min_value=0)
+if st.button("Start Countdown"):
+  for i in range(seconds, 0, -1):
+  st.write(i)
+  time.sleep(1)
+  st.write("Time's up!")
