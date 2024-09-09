a6c5ad89968d5f764ee041aeb7698086d619575c
pathyorkshire <jack.so@pathyorkshire.co.uk>
2024-09-09 16:37:56 +0100

    Update jack.py

--- jack.py
+++ jack.py
@@ -3,6 +3,6 @@ import time
 seconds = st.number_input("Enter countdown time in seconds:", min_value=0)
 if st.button("Start Countdown"):
   for i in range(seconds, 0, -1):
-  st.write(i)
-  time.sleep(1)
-  st.write("Time's up!")
+    st.write(i)
+    time.sleep(1)
+st.write("Time's up!")
